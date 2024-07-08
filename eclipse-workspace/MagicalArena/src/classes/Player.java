2ba0738563daa2b805605e8d429148c23b5b778e
safdarkhan <khandimpal01@gmail.com>
2024-07-08 20:28:37 +0530

    initial Setup of Project

---
+++ eclipse-workspace/MagicalArena/src/classes/Player.java
@@ -0,0 +1,48 @@
+package classes;
+public class Player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health < 0) {
+            health = 0;
+        }
+    }
+
+    public int attackRoll() {
+        return (int)(Math.random() * 6) + 1;
+    }
+
+    public int defenseRoll() {
+        return (int)(Math.random() * 6) + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Player(Health: " + health + ", Strength: " + strength + ", Attack: " + attack + ")";
+    }
+}
